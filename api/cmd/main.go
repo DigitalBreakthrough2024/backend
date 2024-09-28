f150793963edf6c3cb9fcc99ed15f2a96c3534b1
gl1n0m3c <nperewerzew@gmail.com>
2024-09-29 02:15:13 +0300

    ADD docker compose

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -7,13 +7,24 @@ import (
 	"backend/pkg/database"
 	"backend/pkg/log"
 	"fmt"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
+	"time"
 )
 
 func main() {
 	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	logger, loggerFile := log.InitLoggers()
 	defer loggerFile.Close()
